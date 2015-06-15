3e3fbf641a9df95e9c4a092ffda9af4d984abd92
Baris Balic <leafybasil@gmail.com>
2015-06-15 21:14:45 +0100

    Extract some responsibility.

--- lib/repositories/nuget.rb
+++ lib/repositories/nuget.rb
@@ -1,58 +1,110 @@
 class Repositories
+  class Catalogue
+    def self.index
+      Repositories::Base.get_json("https://api.nuget.org/v3/catalog0/index.json")
+    end
+
+    def self.pages(since='2010-01-01', &block)
+      index['items'].each do |item|
+        next unless Time.parse(item['commitTimeStamp']) >= Time.parse(since)
+        yield Page.new(item)
+      end
+    end
+  end
+
+  class Page
+    def initialize(data)
+      @page = Repositories::Base.get_json(data['@id'])
+    end
+
+    def packages(since='2010-01-01', &block)
+      @page['items'].each do |item|
+        next unless Time.parse(item['commitTimeStamp']) > Time.parse(since)
+        yield Package.new(item['@id'])
+      end
+    end
+  end
+
+  class Package
+    def initialize(url)
+      @data = Repositories::Base.get_json(url)
+    end
+
+    def name
+      @data['id']
+    end
+
+    def to_h
+      {
+        name: @data['id'],
+        description: description,
+        homepage: @data['projectUrl'],
+        keywords_array: tags,
+        repository_url: project_url
+      }
+    end
+
+    def versions
+      [{ number: @data['version'], published_at: @data['published'] }]
+    end
+
+    private
+
+    def description
+      @data['description'].blank? ? @data['summary'] : @data['description']
+    end
+
+    def tags
+      Array(@data['tags'])
+    end
+
+    def project_url
+      Repositories::Base.repo_fallback('', @data['projectUrl'])
+    end
+  end
+
   class NuGet < Base
     HAS_VERSIONS = true
     HAS_DEPENDENCIES = true
     URL = 'https://www.nuget.org'
     COLOR = '#178600'
 
-    def self.load_names(limit = nil)
-      segment_index = Repositories::NuGet.get_json "http://preview.nuget.org/ver3-ctp1/islatest/segment_index.json"
-      segment_count = limit || segment_index['segmentNumber'].to_i - 1
-
-      (0..segment_count).to_a.reverse.each do |number|
-        page = Repositories::NuGet.get_json "http://nugetprod0.blob.core.windows.net/ver3-ctp1/islatest/segment_#{number}.json"
-        package_ids = page['entry'].map{|entry| entry['id'] }
-        package_ids.each { |id| REDIS.sadd 'nuget-names', id }
+    class << self
+      def import
+        Catalogue.pages do |page|
+          page.packages do |package|
+            project = Project.find_or_initialize_by({name: package.name, platform: self.name.demodulize})
+            if project.new_record?
+              project.assign_attributes(package.to_h.except(:name))
+              project.save
+            else
+              project.update_attributes(package.to_h.except(:name))
+            end
+            package.versions.each do |version|
+              project.versions.find_or_create_by(version)
+            end
+          end
+        end
       end
-      puts "Loaded all the names"
-    end
 
-    def self.project_names
-      REDIS.smembers 'nuget-names'
-    end
+      def import_recent
+        last_page = REDIS.get('nuget:page')
+        last_project = REDIS.get('nuget:project')
 
-    def self.project(name)
-      h = {
-        name: name
-      }
-      h[:versions] = versions(h)
-      h
-    end
-
-    def self.mapping(project)
-      latest_version = get_json("https://az320820.vo.msecnd.net/registrations-0/#{project[:name].downcase}/index.json")
-      item = latest_version['items'].first['items'].first['catalogEntry']
-
-      {
-        name: project[:name],
-        description: description(item),
-        homepage: item['projectUrl'],
-        keywords_array: Array(item['tags']),
-        repository_url: repo_fallback('', item['projectUrl'])
-      }
-    end
-
-    def self.description(item)
-      item['description'].blank? ? item['summary'] : item['description']
-    end
-
-    def self.versions(project)
-      latest_version = get_json("https://az320820.vo.msecnd.net/registrations-0/#{project[:name].downcase}/index.json")
-      latest_version['items'].first['items'].map do |item|
-        {
-          number: item['catalogEntry']['version'],
-          published_at: item['catalogEntry']['published']
-        }
+        Catalogue.pages(last_page) do |page|
+          page.packages(last_project) do |package|
+            project = Project.find_or_initialize_by({name: package.name, platform: self.name.demodulize})
+            if project.new_record?
+              project.assign_attributes(package.to_h.except(:name))
+              project.save
+            else
+              project.update_attributes(package.to_h.except(:name))
+            end
+            package.versions.each do |version|
+              project.versions.find_or_create_by(version)
+            end
+          end
+        end
       end
     end
   end
